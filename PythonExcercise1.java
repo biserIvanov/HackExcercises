bf2b5da70ae3a2c1f8743ad06438920c8ecf20ba
golfdman <biserivanov08@gmail.com>
2014-09-21 01:34:29 -0700

    Update PythonExcercise1.java

--- PythonExcercise1.java
+++ PythonExcercise1.java
@@ -1,2 +1,23 @@
 HackExcercises
 ==============
+public class ExcerciseOne {
+
+	
+	public static void main(String[] args) {
+		System.out.println(ExOh(""));
+	}
+
+	private static boolean ExOh(String input){
+		int xNum = 0;
+		int oNum = 0;
+		for(int i = 0; i<input.length(); i++){
+			if(input.charAt(i)=='o'){
+				oNum++;
+			}else xNum++;
+		}
+		if(xNum==oNum) return true;
+		else return false;
+		
+	}
+
+}
